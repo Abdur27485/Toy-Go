95a7f0c63ea71826aa7cc61fb817532a491df6b7
Abdur Rahman <abdurrahman27485@gmail.com>
2023-05-18 16:30:46 +0600

    initial state

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Layout from './Layout.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <Layout></Layout>
+  </React.StrictMode>,
+)
