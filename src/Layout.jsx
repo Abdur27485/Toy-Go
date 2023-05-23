737bd55621573ba9d800ed8bc3780c17c1e90724
Abdur Rahman <abdurrahman27485@gmail.com>
2023-05-23 16:15:10 +0600

    primary logo done

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/shared/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/shared/Footer';
 
 const Layout = () => {
     return (
         <div>
             <Navbar />
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
