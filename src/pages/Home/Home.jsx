9bc42466135d0eeca2906e924ae04fb338317eb5
Abdur Rahman <abdurrahman27485@gmail.com>
2023-05-24 10:26:29 +0600

    toysblogs component created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,16 +4,19 @@ import { AuthContext } from '../../providers/AuthProvider';
 import Gallery from '../../components/Home/Gallery';
 import CategoryTabs from '../../components/Home/CategoryTabs';
 import Testimonials from '../../components/Home/Testimonials';
+import ToysBlogs from '../../components/Home/ToysBlogs';
 
 const Home = () => {
     const {changeTitle} = useContext(AuthContext);
     changeTitle('Home')
     return (
         <div>
+                                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
             <Banner></Banner>
             <Gallery></Gallery>
             <CategoryTabs></CategoryTabs>
             <Testimonials></Testimonials>
+            <ToysBlogs></ToysBlogs>
         </div>
     );
 };
