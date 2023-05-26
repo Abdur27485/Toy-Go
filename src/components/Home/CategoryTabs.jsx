960976c50805e838b206e4df496420eb658e7cb7
Abdur Rahman <abdurrahman27485@gmail.com>
2023-05-26 22:02:40 +0600

    final submission

--- src/components/Home/CategoryTabs.jsx
+++ src/components/Home/CategoryTabs.jsx
@@ -52,6 +52,72 @@ const CategoryTabs = () => {
                         }
                     </div>
                 </TabPanel>
+                <TabPanel>
+                    <div className='carousel carousel-center w-full lg:w-full gap-3'>
+                        {
+                            allToys?.filter(toy => toy.subCategory === 'Sports Car').map(toy => {
+                                const { toyName, toyPictureUrl, price, _id } = toy;
+                                return (
+                                    <div className="carousel-item bg-slate-100">
+                                        <div className="group relative block overflow-hidden">
+
+                                            <img
+                                                src={toyPictureUrl}
+                                                alt=""
+                                                className="h-[150px] w-[224px] lg:h-64 lg:w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+                                            />
+
+                                            <div className="relative border border-gray-100 bg-slate-100 p-6 pt-0">
+                                                <h3 className="mt-4 text-lg font-medium text-gray-900">{toyName}</h3>
+
+                                                <p className="mt-1.5 text-sm text-gray-700">${price}</p>
+
+                                                <form className="mt-4">
+                                                    <button className="block w-full rounded bg-yellow-400 px-4 py-2 lg:p-4 text-sm font-medium transition hover:scale-105">
+                                                        View Details
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className='carousel carousel-center w-full lg:w-full gap-3'>
+                        {
+                            allToys?.filter(toy => toy.subCategory === 'Sports Car').map(toy => {
+                                const { toyName, toyPictureUrl, price, _id } = toy;
+                                return (
+                                    <div className="carousel-item bg-slate-100">
+                                        <div className="group relative block overflow-hidden">
+
+                                            <img
+                                                src={toyPictureUrl}
+                                                alt=""
+                                                className="h-[150px] w-[224px] lg:h-64 lg:w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
+                                            />
+
+                                            <div className="relative border border-gray-100 bg-slate-100 p-6 pt-0">
+                                                <h3 className="mt-4 text-lg font-medium text-gray-900">{toyName}</h3>
+
+                                                <p className="mt-1.5 text-sm text-gray-700">${price}</p>
+
+                                                <form className="mt-4">
+                                                    <button className="block w-full rounded bg-yellow-400 px-4 py-2 lg:p-4 text-sm font-medium transition hover:scale-105">
+                                                        View Details
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </TabPanel>
 
 
             </Tabs>
