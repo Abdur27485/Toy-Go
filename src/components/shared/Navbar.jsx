de30b5bf9712d8c0c506f7a86a07b208e8d27c8d
Abdur Rahman <abdurrahman27485@gmail.com>
2023-05-26 08:32:59 +0600

    added dropdown navbar in header

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -24,9 +24,18 @@ const Navbar = () => {
             <div className="mx-auto flex h-16 max-w-screen-2xl items-center justify-between px-3 lg:px-8">
                 <div className="flex items-center gap-4">
 
-                    {/* <button type="button" className="p-2 lg:hidden"> */}
-                        {/* hamburger icon here */}
-                    {/* </button> */}
+                    <div className="dropdown">
+                        <label tabIndex={0} className="">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><Link to='/'>Home</Link></li>
+                                <li><Link to='/allToy'>All Toys</Link></li>
+                                <li><Link to='/myToy'>My Toys</Link></li>
+                                <li><Link to='/addToy'>Add Toy</Link></li>
+                                <li><Link to=''>Blogs</Link></li>
+                        </ul>
+                    </div>
 
                     {/* LOGO */}
                     <Link to='/' className="flex">
